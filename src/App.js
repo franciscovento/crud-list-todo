fd88cef17fb204f8a8a0f71cdf0c09c18fb50b88
fran <francisco.vento@outlook.com>
2021-09-21 20:54:39 -0500

    elimino archivos sin usar

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="bg-gradient-to-r from-green-400 to-blue-500  min-h-screen flex items-center justify-center">
       <TodoContainer />
+      
     </div>
   );
 }
